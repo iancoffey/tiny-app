5d94a29459bb33860d833f9f4b967fff97c69d20
Ian Coffey <ian.coffey@gmail.com>
2015-10-26 10:30:12 -0400

    add redis ability

--- app.rb
+++ app.rb
@@ -1,7 +1,30 @@
 require 'sinatra/base'
+require "redis"
+
+# beginning of kitchen sink tester in ruby
+# goal - test while being as obvious as possible whats wrong - which means as little magic as possible.
+host = ENV["REDIS_HOST"] ||= "localhost"
+port = ENV["REDIS_PORT"] ||=  "6379"
+
+$redis = Redis.new(host: host, port: port)
 
 class TinyApp < Sinatra::Base
-  get '/' do
+  get "/" do
     "OK!!"
+  end
+
+  post "/redis" do
+    if params[:key] and not params[:key].empty?
+      key = params[:key]
+      $redis.set("key", key)
+    end
+
+    "Redis key set to #{key}"
+  end
+
+  get "/redis" do
+    key = $redis.get("key")
+
+    "Redis key is #{key}"
   end
 end
